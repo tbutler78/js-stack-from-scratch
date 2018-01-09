a31102066d75d66fa9e67172c86876933bbad1b3
Tom <thomas.a.butler@gmail.com>
2018-01-08 17:14:32 -0700

    Adding React Helmet

--- src/server/render-app.jsx
+++ src/server/render-app.jsx
@@ -1,44 +1,51 @@
 // @flow
 
+import Helmet from 'react-helmet';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { Provider } from 'react-redux';
+import { StaticRouter } from 'react-router';
 
-import React from 'react'
-import ReactDOMServer from 'react-dom/server'
-import { Provider } from 'react-redux'
-import { StaticRouter } from 'react-router'
+import initStore from './init-store';
+import App from './../shared/app';
+import { APP_CONTAINER_CLASS, STATIC_PATH, WDS_PORT } from '../shared/config';
+import { isProd } from '../shared/util';
 
-import initStore from './init-store'
-import App from './../shared/app'
-import { APP_CONTAINER_CLASS, STATIC_PATH, WDS_PORT } from '../shared/config'
-import { isProd } from '../shared/util'
-
-const renderApp = (location: string, plainPartialState: ?Object, routerContext: ?Object = {}) => {
-    const store = initStore(plainPartialState);
-    const appHtml = ReactDOMServer.renderToString(
-        <Provider store={store}>
-            <StaticRouter location={location} context={routerContext}>
-                <App/>
-            </StaticRouter>
-        </Provider>
-    )
-
-    return (
-        `
+const renderApp = (
+  location: string,
+  plainPartialState: ?Object,
+  routerContext: ?Object = {}
+) => {
+  const store = initStore(plainPartialState);
+  const appHtml = ReactDOMServer.renderToString(
+    <Provider store={store}>
+      <StaticRouter location={location} context={routerContext}>
+        <App />
+      </StaticRouter>
+    </Provider>
+  );
+  const head = Helmet.rewind();
+  return `
         <!doctype html>
         <html>
             <head>
-                <title>FIX ME</title>
+                ${head.title}
+                ${head.meta}
                 <link rel="stylesheet" href="${STATIC_PATH}/css/style.css">
             </head>
             <body>
                 <div class="${APP_CONTAINER_CLASS}">${appHtml}</div>
                 <script>
-                    window.__PRELOADED_STATE__ = ${JSON.stringify(store.getState())}
+                    window.__PRELOADED_STATE__ = ${JSON.stringify(
+                      store.getState()
+                    )}
                 </script>
-                <script src="${isProd ? STATIC_PATH: `http://localhost:${WDS_PORT}/dist`}/js/bundle.js"></script>
+                <script src="${
+                  isProd ? STATIC_PATH : `http://localhost:${WDS_PORT}/dist`
+                }/js/bundle.js"></script>
                 </body>
         </html>
-        `
-    )
-}
+        `;
+};
 
 export default renderApp;
